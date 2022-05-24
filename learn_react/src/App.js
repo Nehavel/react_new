5d8b41c697548feb27d21c59371308beb741c39f
Neha Velmurugan <65886866+NehaVelmurugan@users.noreply.github.com>
2022-05-25 00:53:17 +0530

    Class component

--- learn_react/src/App.js
+++ learn_react/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Greet from "./Greet";
+import Welcome from "./components/Welcome";
 
 function App() {
   return (
     <div className="App">
       {/* call using function name */}
       <Greet />
+      <Welcome />
     </div>
   );
 }
