1fe0a7cb0f52c48f5828887c3850de11b628a36f
Neha Velmurugan <65886866+NehaVelmurugan@users.noreply.github.com>
2022-05-25 00:31:50 +0530

    Functional component

--- learn_react/src/App.js
+++ learn_react/src/App.js
@@ -1,8 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Greet from "./Greet";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {/* call using function name */}
+      <Greet />
+    </div>
+  );
 }
 
 export default App;
---
+++ learn_react/src/Greet.js
@@ -0,0 +1,9 @@
+import React from "react";
+// names export if there is no default export
+function Greet() {
+  return <h1>Helloo Neha</h1>;
+}
+// Named export
+//export const Greet = () => <h1>Nehaaaaaaaa</h1>
+//default export
+export default Greet;
