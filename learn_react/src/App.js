f43027627aea58b8e0c3ebab761681f2124166ca
Neha Velmurugan <65886866+NehaVelmurugan@users.noreply.github.com>
2022-05-28 19:15:13 +0530

    state

--- learn_react/src/App.js
+++ learn_react/src/App.js
@@ -1,15 +1,19 @@
 import "./App.css";
 import Greet from "./components/Greet";
 import Welcome from "./components/Welcome";
-
+import Message from "./components/Message";
 function App() {
   return (
     <div className="App">
       {/* call using function name */}
+      {/* props example */}
       <Greet name="Nehaaaaaaa">
         <p>This is children props</p>
       </Greet>
+      {/* props with class */}
       <Welcome name="Nehaaaaaaa" />
+      {/* state example */}
+      <Message></Message>
     </div>
   );
 }
