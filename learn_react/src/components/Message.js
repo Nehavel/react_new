829d24b039c4691318f7469b4e82c4b879aeb66a
Neha Velmurugan <65886866+NehaVelmurugan@users.noreply.github.com>
2022-05-29 19:12:28 +0530

    event binding

--- learn_react/src/components/Message.js
+++ learn_react/src/components/Message.js
@@ -5,6 +5,10 @@ class Message extends Component {
     this.state = {
       message: "New msg",
     };
+    {
+      /*if commented button is used, binding required here
+  this.changeMessage = this.changeMessage.bind(this)*/
+    }
   }
   changeMessage() {
     this.setState({
@@ -17,6 +21,11 @@ class Message extends Component {
         {/* props in class components */}
         <h1>{this.state.message}</h1>
         <button onClick={() => this.changeMessage()}>Subscribe</button>
+        {/*alternative way of writing*/}
+        <button onClick={this.changeMessage.bind(this)}>Subscribe</button>
+        {/*binding in constructor is needed
+        <button onClick={this.changeMessage}>Subscribe</button>
+        */}
       </div>
     );
   }
